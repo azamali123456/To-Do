8518d9a85273879cf4af694e5ffaaf73626d39a8
zainab-mustafa14 <zainabmustafa178@gmail.com>
2024-08-20 12:29:09 +0500

    changing

--- src/components/TaskInput/index.js
+++ src/components/TaskInput/index.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { v4 as uuidv4 } from "uuid";
-
+import "./TaskInput/index.css";
 function TodoInput({ addTask }) {
   const [description, setDescription] = useState("");
   const [priority, setPriority] = useState("Low");
